1298cde15bc8a16906bb85735eca9f69427387da
Andrew Tan <andrewvmail@gmail.com>
2020-09-01 21:50:48 -0700

    Install Rancher server node

--- after-install-rancher-host.sh
+++ after-install-rancher-host.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config
+
+/etc/init.d/ssh restart
+
+apt-get install curl
+
+curl https://releases.rancher.com/install-docker/19.03.sh | sh
