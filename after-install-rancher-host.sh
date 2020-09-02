3db3c99c31e422f537f3f0033d9c3369315f4485
Andrew Tan <andrewvmail@gmail.com>
2020-09-01 22:24:35 -0700

    Installed docker

--- after-install-rancher-host.sh
+++ after-install-rancher-host.sh
@@ -4,6 +4,16 @@ echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config
 
 /etc/init.d/ssh restart
 
-apt-get install curl
+apt-get upgrade
 
-curl https://releases.rancher.com/install-docker/19.03.sh | sh
+apt-get update
+
+apt install apt-transport-https ca-certificates curl software-properties-common gnupg2
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable edge test nightly"
+
+apt update
+
+apt install docker.io
